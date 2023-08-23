064b738e19b50c883383c1624525b095ed1912f8
AnuragBisht71 <bishtanurag71@gmail.com>
2023-08-23 16:43:17 +0530

    functional components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ let App = () => {
   return (
     <div>
       <h1>This is our first react</h1>
+      <p>Some other html</p>
     </div>
   );
 }
---
+++ src/MyComp.js
@@ -0,0 +1,15 @@
+let MyComp = () => {
+    return (
+        <div>
+            <h1>This is my first react app!!!</h1>
+            <p>Some other html</p>
+            <ul>
+                <li>Home</li>
+                <li>Service</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MyComp;
