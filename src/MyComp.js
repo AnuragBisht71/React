f4853f5c4b673c4ab91363c3066244c029e31d9f
AnuragBisht71 <bishtanurag71@gmail.com>
2023-08-23 17:41:48 +0530

    increment and decrement

--- src/MyComp.js
+++ src/MyComp.js
@@ -21,13 +21,25 @@ import React from "react";
 class MyComp extends React.Component {
 
     state = {
-        someNumber: 4,
+        someNumber: 0,
     };
 
     render = () => {
+
+        console.log("Rendered function executed");
+
         return (
             <div>
                 {/* <h1>This is a Class Based Component</h1> */}
+
+                <button onClick={() => {
+                    this.setState({ someNumber: this.state.someNumber + 1 });
+                }}>Increment</button>
+
+                <button onClick={() => {
+                    this.setState({ someNumber: this.state.someNumber - 1 });
+                }}>Decrement</button>
+
                 <h1>{this.state.someNumber}</h1>
             </div>
         )
